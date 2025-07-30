365b0b5f20f157789946588d819d8f3662683dff
Florian RENARD <florian.renard86@gmail.com>
2025-07-30 13:02:37 +0200

    App initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss', '../styles.scss'],
 })
-export class AppComponent {
-  title = 'angular-test';
-}
+export class AppComponent {}
