bd9a1a6ef713113a456d4a40d431effd79fa99f0
Florian RENARD <florian.renard86@gmail.com>
2025-11-30 01:03:40 +0100

    [DEPENDENCIES] Deprecated replacement

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideToastr } from 'ngx-toastr';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr, ToastNoAnimation } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimations(),
-    provideToastr(),
+    provideToastr({
+      toastComponent: ToastNoAnimation
+    }),
     provideHttpClient(),
   ],
 };
---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+import angular from '@analogjs/vite-plugin-angular'
+
+export default defineConfig({
+  plugins: [angular()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['test.setup.ts'],
+    globals: true,
+    include: ['src/**/*.spec.ts']
+  }
+});
