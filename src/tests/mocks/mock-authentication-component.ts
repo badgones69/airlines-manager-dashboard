b171873e65a5347de63fa72363ed5706183dce35
Florian RENARD <florian.renard86@gmail.com>
2026-06-26 12:46:46 +0200

    [TECHNICAL] Vitest unit tests deleting

--- src/tests/mocks/mock-authentication-component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  template: '<h1>Authentication</h1>',
-})
-export class MockAuthenticationComponent {}
--- src/tests/mocks/mock-list-users-component.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  template: '<h1>List users</h1>',
-})
-export class MockListUsersComponent {}
