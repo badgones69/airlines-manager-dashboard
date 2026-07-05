08657a44c5f8bb20757d2c6f4c2f372fa69e5fb6
Florian RENARD <florian.renard86@gmail.com>
2026-07-06 00:59:57 +0200

    [FEATURE] Menu refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,9 +33,6 @@ export class AppComponent implements OnInit {
 
   public menuOpened: boolean = false;
   public airlineSubMenuExpanded: boolean = false;
-  public userSubMenuExpanded: boolean = false;
-  public hubSubMenuExpanded: boolean = false;
-  public destinationSubMenuExpanded: boolean = false;
   public routeSubMenuExpanded: boolean = false;
 
   /* Injections */
@@ -61,9 +58,6 @@ export class AppComponent implements OnInit {
     /* All submenu closing */
     if (!this.menuOpened) {
       this.airlineSubMenuExpanded = false;
-      this.userSubMenuExpanded = false;
-      this.hubSubMenuExpanded = false;
-      this.destinationSubMenuExpanded = false;
       this.routeSubMenuExpanded = false;
     }
   }
@@ -71,21 +65,6 @@ export class AppComponent implements OnInit {
   /* Airline submenu opening/closing */
   airlineSubMenuToggle(): void {
     this.airlineSubMenuExpanded = !this.airlineSubMenuExpanded;
-  }
-
-  /* User submenu opening/closing */
-  userSubMenuToggle(): void {
-    this.userSubMenuExpanded = !this.userSubMenuExpanded;
-  }
-
-  /* Hub submenu opening/closing */
-  hubSubMenuToggle(): void {
-    this.hubSubMenuExpanded = !this.hubSubMenuExpanded;
-  }
-
-  /* Destination submenu opening/closing */
-  destinationSubMenuToggle(): void {
-    this.destinationSubMenuExpanded = !this.destinationSubMenuExpanded;
   }
 
   /* Route submenu opening/closing */
