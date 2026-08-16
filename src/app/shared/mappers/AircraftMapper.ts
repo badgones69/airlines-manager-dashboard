67efb8b455fe40a04949a349c3fd6e24241c8264
Florian RENARD <florian.renard86@gmail.com>
2026-08-16 02:15:13 +0200

    [FEATURE] List aircrafts

--- src/app/shared/mappers/AircraftMapper.ts
+++ src/app/shared/mappers/AircraftMapper.ts
@@ -1,7 +1,34 @@
+import { Aircraft } from '../dto/Aircraft';
+import { getManufacturerById, getModelById } from '../utils/aviation-utils';
+import { AirportMapper } from './AirportMapper';
 import { FlightMapper } from './FlightMapper';
 
 export class AircraftMapper {
+  public airportMapper: AirportMapper = new AirportMapper();
   public flightMapper: FlightMapper = new FlightMapper();
+
+  /* DB => DTO mapping (aircrafts list) */
+  public aircraftsListFromDB(aircraftsListFromDB: any[]): Aircraft[] {
+    let aircraftsList: Aircraft[] = [];
+
+    for (const aircraftFromDB of aircraftsListFromDB) {
+      aircraftsList.push(this.aircraftFromDB(aircraftFromDB));
+    }
+    return aircraftsList;
+  }
+
+  /* DB => DTO mapping */
+  public aircraftFromDB(aircraftFromDB: any): Aircraft {
+    return {
+      id: aircraftFromDB.aircraftID,
+      uuid: aircraftFromDB.aircraftUUID,
+      registration: aircraftFromDB.aircraftRegistration,
+      manufacturer: getManufacturerById(aircraftFromDB.aircraftManufacturer),
+      model: getModelById(aircraftFromDB.aircraftModel, aircraftFromDB.aircraftManufacturer),
+      homeHub: this.airportMapper.airportFromDB(aircraftFromDB.aircraftHomeHub),
+      flights: this.flightMapper.flightsListFromDB(aircraftFromDB.aircraftFlights),
+    } as Aircraft;
+  }
 
   /* DTO => DB mapping */
   public aircraftToDB(aircraftToDB: any): any {
--- src/app/shared/mappers/FlightMapper.ts
+++ src/app/shared/mappers/FlightMapper.ts
@@ -1,6 +1,33 @@
-import { Flight } from "../dto/Flight";
+import { Flight } from '../dto/Flight';
+import { convertStringTimeInDate } from '../utils/date-utils';
+import { RouteMapper } from './RouteMapper';
 
 export class FlightMapper {
+  public routeMapper: RouteMapper = new RouteMapper();
+
+  /* DB => DTO mapping (flights list) */
+  public flightsListFromDB(flightsListFromDB: any[]): Flight[] {
+    let flightsList: Flight[] = [];
+
+    for (const flightFromDB of flightsListFromDB) {
+      flightsList.push(this.flightFromDB(flightFromDB));
+    }
+    return flightsList;
+  }
+
+  /* DB => DTO mapping */
+  public flightFromDB(flightFromDB: any): Flight {
+    return {
+      id: flightFromDB.flightID,
+      uuid: flightFromDB.flightUUID,
+      number: flightFromDB.flightNumber,
+      route: this.routeMapper.routeFromDB(flightFromDB.flightRoute),
+      takeOff: convertStringTimeInDate(flightFromDB.flightTakeOff, false),
+      landing: convertStringTimeInDate(flightFromDB.flightLanding, true),
+      return: flightFromDB.flightReturn,
+    };
+  }
+
   /* DTO => DB mapping (flights list) */
   public flightsListToDB(flightsListToDB: Flight[]): any[] {
     let flightsList: any[] = [];
